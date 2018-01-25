57bc16bcccb9fad8fd5d4b4fc69079b7b9a46e3e
Cameron Hyman <cameronhyman@Camerons-MacBook-Air.local>
2018-01-25 15:07:35 -0500

    test

---
+++ actor/Age.java
@@ -0,0 +1,21 @@
+package actor;
+
+public class Age {
+int age;
+int height;
+
+	public Age(int c,int b) {
+		age=c;
+		height=b;
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Age age=new Age (17,5);
+		System.out.println(age.age);
+	System.out.println (age.height);
+	}
+}
